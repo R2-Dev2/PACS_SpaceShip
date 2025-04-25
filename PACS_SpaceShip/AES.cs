a324b644c3a28c1180ce41bd77962a0d50516c85
DSSaumell <dsegurasmll@gmail.com>
2025-04-25 17:38:03 +0200

    feat: partial AES

---
+++ PACS_SpaceShip/AES.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Data;
+using DataAccess;
+
+namespace PACS_SpaceShip
+{
+    class AES
+    {
+        AccesADades accesADades = new AccesADades("SecureCore");
+
+        private void GenerateIVAES()
+        {
+            string xmlPublicKey;
+
+            using (Aes myAes = Aes.Create())
+            {
+                string keyEncripted;
+                string IVEncripted;
+                myAes.GenerateKey();
+                myAes.GenerateIV();
+
+                xmlPublicKey = RSA.RSA.GetPlanetPublickey("RAKA");
+
+                keyEncripted = RSA.RSA.EncryptRSA(myAes.Key, xmlPublicKey);
+                IVEncripted = RSA.RSA.EncryptRSA(myAes.IV, xmlPublicKey);
+
+                //TODO: ENVIAR LES DADES AL PLANETA
+            }
+        }
+
+        private void EncryptAES(Aes myAes, byte[] deliveryDataPdf)
+        {
+            byte[] encryptedData;
+
+            using (MemoryStream msInput = new MemoryStream(deliveryDataPdf))
+            using (MemoryStream msOutput = new MemoryStream())
+            using (CryptoStream cryptoStream = new CryptoStream(msOutput, myAes.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                msInput.CopyTo(cryptoStream);
+                cryptoStream.FlushFinalBlock();
+                encryptedData = msInput.ToArray();
+            }
+
+            //TODO: ENVIAR LES DADES AL PLANETA
+        }
+
+        private void GetDeliveryDataPdf(string codeDelivery)
+        {
+            Dictionary<string, string> dictPdf = new Dictionary<string, string>();
+            dictPdf.Add("codeDeliveryData", codeDelivery);
+
+            DataSet dts = accesADades.ExecutaCerca("DeliveryDataPdf", dictPdf);
+
+            byte[] deliveryDataPdf = File.ReadAllBytes(dts.Tables[0].Rows[0]["DocumentPdf"].ToString());
+        }
+    }
+}
---
+++ RSA/RSA.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Xml;
+using System.Data;
+using DataAccess;
+
+namespace RSA
+{
+    public static class RSA
+    {
+        private static string keyName;
+        private static string xmlPublicKeyPath = "./planetPublicKey.xml";
+        private static XmlDocument doc = new XmlDocument();
+        static AccesADades accesADades = new AccesADades("SecureCore");
+        private static string query = "Select * From PlanetKeys";
+
+        private static string GetCodeFromXml(string codeType)
+        {
+            string sendCode = "";
+
+            doc.Load("launchConfig.xml");
+            XmlNodeList tcpSetingsList = doc.GetElementsByTagName("launchConfig");
+            foreach (XmlNode tcpNode in tcpSetingsList)
+            {
+                foreach (XmlNode childNode in tcpNode.ChildNodes)
+                {
+                    if (childNode.Name == codeType)
+                    {
+                        sendCode = childNode.InnerText;
+                        break;
+                    }
+                }
+            }
+
+            return sendCode;
+        }
+
+        private static string GetIdPlanet(string codePlanet)
+        {
+            string idPlanet;
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("codePlanet", codePlanet);
+
+            DataSet dataset = accesADades.ExecutaCerca("Planets", dict);
+            idPlanet = dataset.Tables[0].Rows[0]["idPlanet"].ToString();
+
+            return idPlanet;
+        }
+
+        public static void GenerateKeys(string codePlanet)
+        {
+            string idPlanet = GetIdPlanet(codePlanet);
+
+            CspParameters cspp = new CspParameters();
+            keyName = codePlanet;
+
+            cspp.KeyContainerName = keyName;
+
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cspp);
+            rsa.PersistKeyInCsp = true;
+
+            string publicKey = rsa.ToXmlString(false);
+
+            Dictionary<string, string> dictKeys = new Dictionary<string, string>();
+            dictKeys.Add("idPlanet", idPlanet);
+
+            DataSet datasetKeys = accesADades.ExecutaCerca("PlanetKeys", dictKeys);
+
+            if (datasetKeys.Tables[0].Rows.Count > 0)
+            {
+                datasetKeys.Tables[0].Rows[0]["XMLKey"] = publicKey;
+            }
+            else
+            {
+                DataRow row = datasetKeys.Tables[0].NewRow();
+
+                row["XMLKey"] = publicKey;
+                row["idPlanet"] = idPlanet;
+
+                datasetKeys.Tables[0].Rows.Add(row);
+            }
+
+            accesADades.Actualitzar(query, datasetKeys);
+
+            WriteXmlPublicKey(codePlanet);
+        }
+
+        public static string GetPlanetPublickey(string codePlanet)
+        {
+            string idPlanet = GetIdPlanet(codePlanet);
+            Dictionary<string, string> dictKeys = new Dictionary<string, string>();
+            dictKeys.Add("idPlanet", idPlanet);
+
+            DataSet datasetKeys = accesADades.ExecutaCerca("PlanetKeys", dictKeys);
+            string xmlPublicKey = datasetKeys.Tables[0].Rows[0]["XMLKey"].ToString();
+
+            return xmlPublicKey;
+        }
+
+        private static void WriteXmlPublicKey(string codePlanet)
+        {
+            string xmlPublicKey = GetPlanetPublickey(codePlanet);
+            File.WriteAllText(xmlPublicKeyPath, xmlPublicKey);
+        }
+
+        public static string EncryptRSA(byte[] dataToEncrypt, string xmlPublicKey)
+        {
+            string dataEncrypted;
+            RSACryptoServiceProvider rsaEnc = new RSACryptoServiceProvider();
+            rsaEnc.FromXmlString(xmlPublicKey);
+
+
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+
+            byte[] encryptedData = rsaEnc.Encrypt(dataToEncrypt, false);
+
+            dataEncrypted = ByteConverter.GetString(encryptedData);
+
+            return dataEncrypted;
+        }
+    }
+}
